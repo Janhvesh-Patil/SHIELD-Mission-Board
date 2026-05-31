53445654be97f43b6ae6b6fa46fe0971cb03f42d
Janhvesh-Patil <janhveshspatil25@gmail.com>
2026-05-31 23:17:44 +0530

    HeroDao.java files is ready

--- src/main/java/org/shield/dao/HeroDao.java
+++ src/main/java/org/shield/dao/HeroDao.java
@@ -1,4 +1,32 @@
 package org.shield.dao;
 
+import org.shield.db.DatabaseConnection;
+import org.shield.model.Hero;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HeroDao {
+    public List<Hero> findAll() {
+        String query = "SELECT hero_id, hero_name FROM hero;";
+        List<Hero> heroes =new ArrayList<>();
+        try (var connection = DatabaseConnection.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            ) {
+
+            while (resultSet.next()) {
+                Hero hero = new Hero(resultSet.getInt("hero_id"),
+                        resultSet.getString("hero_name"));
+                heroes.add(hero);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return heroes;
+    }
 }
